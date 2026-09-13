dd4c183699f35674342a1e743d0014b0257d712c
KARDIACblade <bladescorpio@gmail.com>
2026-09-13 09:37:29 -0500

    gato centrado y comida en la esquina inferior derecha iniciando variables en cero y variables constantes

--- cazando.js
+++ cazando.js
@@ -1,14 +1,45 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
+//variables gato rectangulo
 let alturaRect = 50;
 let largoRect = 100;
 
 let x = canvas.width/2 - largoRect/2;
 let y = canvas.height/2 - alturaRect/2
 
+//variables comida cuadrado
+let a = 50
+
+// iniciar variables en cero
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+// variables consantes
+const ALTO_GATO = 50;
+const ANCHO_GATO = 100;
+const  ALTO_COMIDA = 50;
+const ANCHO_COMIDA = 50;
+// iniciar grafica con variables iniciales
 
  function graficarGato (){
     ctx.fillStyle = "green";
-    ctx.fillRect(x,y, largoRect,alturaRect);
+    ctx.fillRect(gatoX,gatoY, ANCHO_GATO,ALTO_GATO);
+ }
+ function graficarComida(){
+    ctx.fillStyle = "red";
+    ctx.fillRect(comidaX,comidaY, ANCHO_COMIDA,ALTO_COMIDA);
+    // ctx.fillRect(canvas.width-ANCHO_COMIDA,0, ANCHO_COMIDA,ALTO_COMIDA);
+ }
+// iniciar grafica gato centrado  y comida a la derecha
+ function iniciarJuego(){
+   
+ gatoX = canvas.width/2 - ANCHO_GATO/2;
+ gatoY = canvas.height/2 - ALTO_GATO/2;
+ comidaX=canvas.width-ANCHO_COMIDA;
+ comidaY=canvas.height-ALTO_COMIDA;
+
+    graficarGato ();
+    graficarComida();
  }
